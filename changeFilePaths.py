47c6b9df9f09bad41f35cfecfe955aaade54d54f
Travis <onebean91@yahoo.com>
2020-11-26 23:04:24 -0500

    Add option to auto mount removable media

--- changeFilePaths.py
+++ changeFilePaths.py
@@ -1,6 +1,7 @@
 import platform
 from pathlib import Path
 import json
+import os
 from termcolor import colored
 
 configFile = open('pmConfig.json', 'r')
@@ -18,9 +19,17 @@ else:
 def changeRmMediaPath():
     rmFileLoopStop = True
     while rmFileLoopStop:
-        rm_media_path = Path(input("Enter drive letter or path for removable media: ") + rmDiskSuffix)
-        print("The path for removable media is:", rm_media_path, "\n")
-        if rm_media_path.exists():
+        rm_media_path = input("Enter drive letter or path for removable media: ") + rmDiskSuffix
+        if Path(rm_media_path).exists():
+            # Assume if removable drive is empty, then the drive isn't mounted
+            if not os.listdir(Path(rm_media_path)):
+                option_1 = input("Would you like to mount this removable drive? (y/n): ")
+                if option_1 == "Y" or option_1 == "y":
+                    drive_letter = rm_media_path[-1]
+                    # The following command only works for Linux and WSL
+                    os.system(f"sudo mount -t drvfs {drive_letter}: /mnt/{drive_letter}")
+                    print(colored(f"{rm_media_path} mounted successfully!\n"))
+            print("The path for removable media is:", rm_media_path, "\n")
             filePath["rm_media_path"] = str(rm_media_path)
             configFile = open('pmConfig.json', 'w')
             json.dump(filePath, configFile)
@@ -31,9 +40,11 @@ def changeRmMediaPath():
 
 
 def checkConfigPaths():
-    if filePath["rm_media_path"] == "":
+    try:
+        if filePath["rm_media_path"] == "" or not os.listdir(filePath["rm_media_path"]):
+            changeRmMediaPath()
+    except:
         changeRmMediaPath()
-
 
 def get_rm_media_path():
     rm_media_output_file = filePath["rm_media_path"] + filePathSeparator + "secret.key"
